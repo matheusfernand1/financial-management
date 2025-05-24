ef59347a26980f4db7e1269fcde794b3b3d35798
matheusfernand1 <matheusfernandpro@gmail.com>
2025-05-24 00:22:45 -0300

    add custom exception classes for account management

---
+++ src/main/java/com/ngbilling/financial_management/exception/ContaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.ngbilling.financial_management.exception;
+
+public class ContaNaoEncontradaException extends RuntimeException {
+    public ContaNaoEncontradaException(String message) {
+        super(message);
+    }
+}
